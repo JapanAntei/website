cb70d769cadfb66ce9864a89e35bae6db4ec9bb5
suw-ankosoba <196719433+suw-ankosoba@users.noreply.github.com>
2025-01-28 15:22:42 +0900

    とりあえずPush

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  /*build: {
+    rollupOptions: {
+      input: {
+        sample1: path.resolve(import.meta.dirname, 'src/sample1/index.html'),
+        sample2: path.resolve(import.meta.dirname, 'src/sample2/index.html'),
+      },
+    },
+  },*/
+  root: 'src',
+  base: '/',
+  publicDir: 'public',
+    build: {
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        sample1: path.resolve(import.meta.dirname, 'src/index.html'),
+        sample2: path.resolve(import.meta.dirname, 'src/aaaa/index.html'),
+      },
+    }
+  },
+  // 絶対パスか、`root` からの相対パスで指定する
+  envDir: '../',
 })
